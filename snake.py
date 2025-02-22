b2531514e83bb26ab9d162ac2814ad334f5c2d90
Ulises Gomez <usg@arizona.edu>
2025-02-21 22:15:54 -0800

    set bondries for snake to not exceed the screen width and height

--- snake.py
+++ snake.py
@@ -15,12 +15,32 @@ screen = pygame.display.set_mode((width, height))
 fps = pygame.time.Clock()
 
 
-# React(left, top, width, height) -> Rect
-snakeHead = pygame.Rect(250, 150, 10, 10)
 apple = None # pygmae.Rect()
 
 
+class Snake:
+    def __init__(self):
+        # React(left, top, width, height) -> Rect
+        self.head = pygame.Rect(250, 150, 10, 10)
+        self.body = [ self.head ]
+        self.speed = 5    
+
+    def moveUp(self):
+        self.head.y -= self.speed
+
+    def moveDown(self):
+        self.head.y += self.speed
+
+    def moveLeft(self):
+        self.head.x -= self.speed
+
+    def moveRight(self):
+        self.head.x += self.speed
+
+
 def main():
+    snake = Snake()
+
     running = True
     while running:
         screen.fill((0, 0, 0))
@@ -29,16 +49,16 @@ def main():
                 running = False
         
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
-            snakeHead.y -= 5
-        if keys[pygame.K_DOWN]:
-            snakeHead.y += 5
-        if keys[pygame.K_LEFT]:
-            snakeHead.x -= 5
-        if keys[pygame.K_RIGHT]:
-            snakeHead.x += 5
+        if keys[pygame.K_UP] and snake.head.top > 0:
+            snake.moveUp()
+        if keys[pygame.K_DOWN] and snake.head.bottom < height:
+            snake.moveDown()
+        if keys[pygame.K_LEFT] and snake.head.left > 0:
+            snake.moveLeft()
+        if keys[pygame.K_RIGHT] and snake.head.right < width:
+            snake.moveRight()
 
-        pygame.draw.rect(screen, (68, 214, 44), snakeHead)
+        pygame.draw.rect(screen, (68, 214, 44), snake.head)
         pygame.display.update()
         fps.tick(60)
 
