29f4862f54a3e527eb8fb96f58ea03307fd6cde8
Ulises Gomez <usg@arizona.edu>
2025-02-22 16:42:47 -0800

    added collision detection which triggers the snake to grow

--- snake.py
+++ snake.py
@@ -6,6 +6,7 @@ The game resets if the snake collides with the walls and itself.
 
 import pygame
 import sys
+import random
 
 # Reference: https://en.wikipedia.org/wiki/Snake_(video_game_genre)
 
@@ -22,41 +23,101 @@ frames = 60
 fps = pygame.time.Clock()
 
 
-apple = None # pygmae.Rect()
-
-
 class Snake:
     def __init__(self):
         # React(left, top, width, height) -> Rect
-        self.vertebrae = [ pygame.Rect(50, 150, 10, 10) ]
+        self.head = []
         self.speed = 3    
         self.color = (68, 214, 44)
 
+        self.head.append(pygame.Rect(50, 150, 10, 10))
+
+    def getHead(self):
+        return self.head[0]
+
+    def getTop(self):
+        snakeHead = self.getHead()
+        return snakeHead.top
+
+    def getBottom(self):
+        snakeHead = self.getHead()
+        return snakeHead.bottom
+        
+    def getLeft(self):
+        snakeHead = self.getHead()
+        return snakeHead.left
+
+    def getRight(self):
+        snakeHead = self.getHead()
+        return snakeHead.right
+
     def moveUp(self):
-        for col in self.vertebrae:        
-            col.y -= self.speed
+        self.update()
+        self.head[0].y -= self.speed        
 
     def moveDown(self):
-        for col in self.vertebrae:        
-            col.y += self.speed
+        self.update()
+        self.head[0].y += self.speed
 
     def moveLeft(self):
-        for col in self.vertebrae: 
-            col.x -= self.speed
+        self.update()
+        self.head[0].x -= self.speed
 
     def moveRight(self):
-        # self.head.x += self.speed
-        for col in self.vertebrae: 
-            col.x += self.speed
+        self.update()
+        self.head[0].x += self.speed
+
+    def add(self, direction):
+        snakeTail = self.head[-1]
+        newSnakeTail = pygame.Rect(snakeTail.left, snakeTail.top, 10, 10) # can you make a copy of the reference 
+
+        if direction == 'u':
+            newSnakeTail.top += 10
+        elif direction == 'd':
+            newSnakeTail.top -= 10
+        elif direction == 'l':
+            newSnakeTail.left += 10
+        elif direction == 'r':
+            newSnakeTail.left -= 10
+        
+        self.head.append(newSnakeTail)   
+
+
+    def update(self):
+        x_curr, y_curr = self.head[0].x, self.head[0].y
+        for current in self.head[1:]:
+            x_prev, y_prev = current.x, current.y
+            current.x, current.y = x_curr, y_curr
+            x_curr, y_curr = x_prev, y_prev
+
 
     def draw(self, screen):
-        for col in self.vertebrae:        
-            pygame.draw.rect(screen, self.color, col)
+        for current in self.head:
+            pygame.draw.rect(screen, self.color, current)
 
+    def reset(self):
+        del self.head[1:]
+
+        snakeHead = self.getHead()
+        snakeHead.x, snakeHead.y = 50, 150
+
+
+class Apple:
+    def __init__(self):
+        self.apple = pygame.Rect(250, 150, 10, 10)
+        self.color = (255, 0, 0)
+
+    def draw(self, screen):
+        pygame.draw.ellipse(screen, self.color, self.apple)
+    
+    def reset(self):
+        self.apple.x, self.apple.y = 250, 150
 
 def main():
     snake = Snake()
+    apple = Apple()
     slither = None
+    direction = None
     # we need to set the current direction because snake can't go the opposite direction
 
     running = True
@@ -69,19 +130,43 @@ def main():
                 running = False
         
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP] and snake.vertebrae[0].top > 0:
+
+        if keys[pygame.K_UP] and snake.getTop() > 0 and direction != 'd':
             slither = snake.moveUp
-        if keys[pygame.K_DOWN] and snake.vertebrae[0].bottom < height:
+            direction = 'u'
+
+        if keys[pygame.K_DOWN] and snake.getBottom() < height and direction != 'u':
             slither = snake.moveDown
-        if keys[pygame.K_LEFT] and snake.vertebrae[0].left > 0:
+            direction = 'd'
+
+        if keys[pygame.K_LEFT] and snake.getLeft() > 0 and direction != 'r':
             slither = snake.moveLeft
-        if keys[pygame.K_RIGHT] and snake.vertebrae[0].right < width:
+            direction = 'l'
+
+        if keys[pygame.K_RIGHT] and snake.getRight() < width and direction != 'l':
             slither = snake.moveRight
+            direction = 'r'
+
+        if snake.getTop() <= 0 or snake.getBottom() >= height or snake.getLeft() <= 0 or snake.getRight() >= width:
+            # resets game 
+            snake.reset() # use inheritance to update apple when snake resets
+            apple.reset()
+            slither = None
+            direction = None
 
         if slither is not None:
             slither()
 
         snake.draw(screen)
+        apple.draw(screen)
+
+        snakeHead = snake.getHead()
+        if snakeHead.colliderect(apple.apple):
+            snake.add(direction)
+
+            apple.apple.x = random.randrange(0, 500, 10)
+            apple.apple.y = random.randrange(0, 300, 10)
+
         pygame.display.update()
         fps.tick(frames)
 
